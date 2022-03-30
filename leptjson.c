65f35d1db58892870820b1f57e0a201a2ffd904c
wonderzyp <2671078523@qq.com>
2022-03-30 17:52:04 +0800

    添加解析数字的错误检查

---
+++ leptjson.c
@@ -0,0 +1,147 @@
+#include "leptjson.h"
+#include <assert.h>
+#include <stdlib.h>
+
+#define EXPECT(c, ch)  do { assert(*c->json == (ch)); c->json++; } while(0)
+
+#define ISDIGIT(ch) ((ch) >= '0' && (ch)<='9')
+#define ISDIGIT1TO9(ch) ((ch) >='1' && (ch) <='9')
+
+
+typedef struct {
+  const char* json;
+}lept_context;
+
+
+/**
+* @brief 解析器
+* ****************************************************************************************/
+
+// 解析空白：空白由零或多个空格符、制表符、换行符、回车符组成
+static void lept_parse_whitespace(lept_context* c){
+  const char *p = c->json;
+  while(*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
+    p++;
+  c->json = p;
+}
+
+// 解析null
+static int lept_parse_null(lept_context* c, lept_value* v){
+  EXPECT(c, 'n');
+  if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
+    return LEPT_PARSE_INVALID_VALUE;
+
+  c->json += 3;
+  v->type = LEPT_NULL;
+  return LEPT_PARSE_OK;
+}
+
+// 解析true
+static int lept_parse_true(lept_context* c, lept_value* v){
+  EXPECT(c, 't');
+  if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+    return LEPT_PARSE_INVALID_VALUE;
+
+  c->json += 3;
+  v->type = LEPT_TRUE;
+  return LEPT_PARSE_OK;
+}
+
+// 解析false
+static int lept_parse_false(lept_context* c, lept_value* v){
+  EXPECT(c, 'f');
+  if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+    return LEPT_PARSE_INVALID_VALUE;
+  
+  c->json += 4;
+  v->type = LEPT_FALSE;
+  return LEPT_PARSE_OK;
+}
+
+
+// 现实值可以是null, false或true
+static int lept_parse_value(lept_context* c, lept_value* v){
+  switch (*c->json){
+    case 'n':  return lept_parse_null(c, v);
+    case 't':  return lept_parse_true(c, v);
+    case 'f':  return lept_parse_false(c, v);
+    case '\0': return LEPT_PARSE_EXPECT_VALUE;  // json只有空白
+    default:   return lept_parse_number(c,v); //
+  };
+}
+
+static int lept_parse_number(lept_context*c, lept_value* v){
+
+  const char* p = c->json;
+
+  if (*p == '-') ++p;
+
+  if (*p == '0') ++p;
+  else {
+    if (!ISDIGIT1TO9(*p)) return LEPT_PARSE_INVALID_VALUE;
+    for(; ISDIGIT(*p); ++p);
+  }
+
+  if (*p == '.') {
+    ++p;
+    if (!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
+    for(; ISDIGIT(*p); ++p);
+  }
+
+  if (*p == 'e' || *p == 'E'){
+    ++p;
+    if (*p == '+' || *p == '-') ++p;
+    if (!ISDIGIT(*p)) return LEPT_PARSE_INVALID_VALUE;
+    for(; ISDIGIT(*p); ++p);
+  }
+  
+  v->n = strtod(c->json, NULL);
+  v->type = LEPT_NUMBER;
+  c->json = p;
+  return LEPT_PARSE_OK;
+  // char* end;
+
+  // v->n = strtod(c->json, &end);
+  // if (c->json == end){
+  //   return LEPT_PARSE_INVALID_VALUE;
+  // }
+  // c->json = end;
+  // v->type = LEPT_NUMBER;
+  // return LEPT_PARSE_OK;
+}
+
+
+int lept_parse(lept_value* v, const char* json){
+  lept_context c;
+  int ret;
+  assert(v != NULL); // why?
+  c.json = json;
+  v->type = LEPT_NULL;
+  lept_parse_whitespace(&c);
+  
+  // value后的空白之后不可存在值
+  // 对解析值后的json调用lept_parse_whitespace()解析
+  if ((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
+    lept_parse_whitespace(&c);
+    if (*c.json != '\0')
+      ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+  }
+  return ret;
+}
+
+/**
+* @brief 获取类型
+* ****************************************************************************************/
+lept_type lept_get_type(const lept_value* v){
+  assert(v != NULL);
+  return v->type;
+}
+
+
+// 使用断言，确保用户调用时类型满足: type==LEPT_NUMBER
+double lept_get_number(const lept_value* v){
+  assert(v != NULL && v->type == LEPT_NUMBER);
+  return v->n;
+}
+
+
