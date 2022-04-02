a23b3a00ce125fe9dfd36bf831c6c013d4cdac69
wonderzyp <2671078523@qq.com>
2022-04-02 17:33:22 +0800

    添加转义字符, passing

--- leptjson.c
+++ leptjson.c
@@ -99,6 +99,20 @@ static int lept_parse_string(lept_context* c, lept_value* v) {
                 lept_set_string(v, (const char*)lept_context_pop(c, len), len);
                 c->json = p;
                 return LEPT_PARSE_OK;
+            case '\\':   // 如果当前字符为 '\'
+                switch(*p++){
+                    case '\"': PUTC(c, '\"'); break;
+                    case '\\': PUTC(c, '\\'); break;
+                    case '/' : PUTC(c, '/');  break;
+                    case 'b' : PUTC(c, '\b'); break;
+                    case 'n' : PUTC(c, '\n'); break;
+                    case 't' : PUTC(c, '\t'); break;
+                    case 'f' : PUTC(c, '\f'); break;
+                    case 'r' : PUTC(c, '\r'); break;
+                    default:
+                        c->top = head;
+                        return LEPT_PARSE_INVALID_STRING_ESCAPE;
+                }break;
             case '\0':
                 c->top = head;
                 return LEPT_PARSE_MISS_QUOTATION_MARK;
