dd0b7543495c0f5263e1b730bebe2f40d069b123
wonderzyp <2671078523@qq.com>
2022-04-03 18:31:19 +0800

    加入检测代理对功能

--- leptjson.c
+++ leptjson.c
@@ -126,7 +126,7 @@ static void lept_encode_utf8(lept_context* c, unsigned u) {
 
 static int lept_parse_string(lept_context* c, lept_value* v) {
     size_t head = c->top, len;
-    unsigned u;
+    unsigned u,u2;
     const char* p;
     EXPECT(c, '\"');
     p = c->json;
@@ -151,7 +151,18 @@ static int lept_parse_string(lept_context* c, lept_value* v) {
                     case 'u':
                         if (!(p = lept_parse_hex4(p, &u)))
                             STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_HEX);
-                        /* \TODO surrogate handling */
+
+                        if (u >= 0xD800 && u<=0xDBFF){
+                            if (*p++ != '\\')
+                                STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_SURROGATE);
+                            if (*p++ != 'u')
+                                STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_SURROGATE);
+                            if (!(p = lept_parse_hex4(p, &u2)))
+                                STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_HEX);
+                            if (u2< 0xDC00 || u2 > 0xDFFF) // 不符合utf规定
+                                STRING_ERROR(LEPT_PARSE_INVALID_UNICODE_SURROGATE);
+                            u = (((u - 0xD800) << 10) | (u2 - 0xDC00)) + 0x10000; // 符合计算公式
+                        }
                         lept_encode_utf8(c, u);
                         break;
                     default:
